af0e891263a1c3f3c98f7d33bc6ad7d561d51141
Gaurav Sahni <sendtogaurav@yahoo.com>
2020-04-10 08:22:15 -0400

    Cleanup

--- README.md
+++ README.md
@@ -1,56 +1,20 @@
-# Whole Foods and Amazon Fresh Delivery Slot Automated Script
-
-Yes, amid COVID-19 trying to get Whole Foods and Amazon Fresh delivery slots can get cumbersome. To free you off the constant hassle of checking for slots (and almost never finding one), this automated script can notify you (yes notifies you verbally, so you can go about your tasks) of when new delivery slots open.
-
+# Amazon Fresh Delivery Slot Automated Script
 
 ## Usage:
-Supports **MacOS, Linux, and Windows**.
+Supports **MacOS, Linux with Chrome**.
 
-Note, use the ```whole_foods_delivery_windows.py``` for Windows. Also, only for this OS, you'll have to install an additional package ```winsound```
-
-The script works on **Chrome** (```whole_foods_delivery_slot_chrome.py``` for Whole Foods) (```amazon_fresh_delivery_slot_chrome.py``` for Amazon Fresh) and **FireFox** (```whole_foods_delivery_slot_firefox.py```) for now. 
+The script works on **Chrome** (```amazon_fresh_delivery_slot_chrome.py```). 
 It initializes a  webdriver, for which if you don't have one install it from: https://chromedriver.chromium.org/ for Chrome and https://github.com/mozilla/geckodriver/releases for FireFox.
 
 You'll have to update the path of the this installed webdriver under: ```python driver = webdriver.Chrome()``` if its not the default location your OS needs. Similarly, for FireFox ```python driver = webdriver.Firefox(executable_path="<your-webdriver-path>")```
 
-Script was written on Python 2.7.10
-
-
-
 
 _The script works after you have added all the items to your cart! Note, have your cart ready before running this script! Also, please don't let your computer sleep. Let your computer do the work, while you sleep_
 
-
-
 ### After you clone the project:
-_Walkthrough for Chrome for Whole Foods, follow same steps if running on FireFox with the FireFox script_
 
 1. Run the requirements.txt (```$ pip install -r requirements.txt```)
-2. Run whole_foods_delivery_slot_chrome.py (``` $ python whole_foods_delivery_slot_chrome.py```)
-3. The first time you run this script, Whole Foods cart will ask you to login. After you login, go to the "Shipping and Payment" window. Its titled: _Schedule your order_. Leave the script running.
-4. Get a nice warm Tea, browse reddit, do something on Xbox, etc.
-5. Once a slot opens the script will verbally notify you of an open slot.
-6. Proceed to checkout once you select a time slot. Stay Safe!
-
-### Non-coding background help:
-For people with no background with coding there is this amazing post out there:
-https://www.notion.so/using-pcomputo-s-script-to-find-whole-foods-delivery-slots-acbb6d71ef934da7b6822b1df451a11c
-
-### Visual Aid
-
-> __Screen 1__
-![alt text](https://github.com/pcomputo/Whole-Foods-Delivery-Slot/blob/master/instruction_img/step1.png)
-
-> __Screen 2__
-![alt text](https://github.com/pcomputo/Whole-Foods-Delivery-Slot/blob/master/instruction_img/step2.png)
-
-> __Screen 3__
-![alt text](https://github.com/pcomputo/Whole-Foods-Delivery-Slot/blob/master/instruction_img/step3.png)
-
-> __Screen 4__
-![alt text](https://github.com/pcomputo/Whole-Foods-Delivery-Slot/blob/master/instruction_img/step4.png)
-
-> __Screen 5: Leave script running on this screen!__
-![alt text](https://github.com/pcomputo/Whole-Foods-Delivery-Slot/blob/master/instruction_img/step5_new.png)
-
-
+2. Run amazon_fresh_delivery_slot_chrome.py 
+3. The first time you run this script, amazon will ask you to login. After you login, go to the "Shipping and Payment" window. Its titled: _Schedule your order_. Leave the script running.
+4. Once a slot opens the script will verbally notify you of an open slot.
+5. Proceed to checkout once you select a time slot. Stay Safe!
--- amazon_fresh_delivery_slot_chrome.py
+++ amazon_fresh_delivery_slot_chrome.py
@@ -1,7 +1,7 @@
 import bs4
-
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
+
+from datetime import datetime,timedelta
 
 import sys
 import time
@@ -13,52 +13,72 @@ def getWFSlot(productUrl):
        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
    }
 
-   driver = webdriver.Chrome()
-   driver.get(productUrl)           
+
+   options = webdriver.ChromeOptions()
+   options.add_argument("user-data-dir=selenium")
+
+   driver = webdriver.Chrome(chrome_options=options)
+
+   #cook = driver.get_cookies();
+   driver.get(productUrl)
    html = driver.page_source
-   soup = bs4.BeautifulSoup(html)
+   soup = bs4.BeautifulSoup(html,"html.parser")
    time.sleep(60)
    no_open_slots = True
 
+   t1 = 'No doorstep delivery windows are available for Today, Apr 9.'
+   t2 = 'No doorstep delivery windows are available for Friday, Apr 10.'
+   t3 = 'No doorstep delivery windows are available for Saturday, Apr 11.'
+   t4 = 'No doorstep delivery windows are available for Sunday, Apr 12.'
+   t5 = 'No doorstep delivery windows are available for Monday, Apr 13.'
+
+
    while no_open_slots:
       driver.refresh()
-      print("refreshed")
+
+      t1date = datetime.today()
+      t1 = 'No doorstep delivery windows are available for Today'
+      t2 = 'No doorstep delivery windows are available for ' + str((t1date + timedelta(days=1)).strftime('%A'))
+      t3 = 'No doorstep delivery windows are available for ' + str((t1date + timedelta(days=2)).strftime('%A'))
+      t4 = 'No doorstep delivery windows are available for ' + str((t1date + timedelta(days=3)).strftime('%A'))
+
+      print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), " refreshed!!!!")
       html = driver.page_source
-      soup = bs4.BeautifulSoup(html)
-      time.sleep(2)
+      soup = bs4.BeautifulSoup(html,"html.parser")
 
-      no_open_slots = "No doorstep delivery windows are available for"
+      slot_patterns = ['Next available', '1-hour delivery windows', '2-hour delivery windows']
+
+      t1pos = soup.text.find(t1)
+      if t1pos < 0:
+         print('SLOTS OPEN TODAY!')
+         os.system('say "Slots for delivery opened today!"')
+
+      t2pos = soup.text.find(t2)
+      if t2pos < 0:
+         print('SLOTS OPEN!! ', t2.split()[-1])
+         os.system('say "Slots for delivery opened t2!"')
+
+      t3pos = soup.text.find(t3)
+      if t3pos < 0:
+         print('SLOTS OPEN ! ', t3.split()[-1])
+         os.system('say "Slots for delivery opened t3!"')
+
+      t4pos = soup.text.find(t4)
+      if t4pos < 0:
+         print('SLOTS OPEN ! ', t4.split()[-1])
+         os.system('say "Slots for delivery opened t4!"')
+
+
       try:
-         no_slots_from_web = driver.find_element_by_xpath('/html/body/div[5]/div/div/div[2]/div/div/form/div[3]/div[4]/div/div[2]/div[2]/div[6]/div/div[2]/div/div[2]/div/div[20]/div[1]/div[1]/div/div/div/span').text
-         if no_open_slots in no_slots_from_web:
-            pass
-         else:
+         no_slot_pattern = 'No delivery windows available. New windows are released throughout the day.'
+         if no_slot_pattern == soup.find('h4', class_ ='a-alert-heading').text:
+            print("NO SLOTS!")
+      except AttributeError: 
             print('SLOTS OPEN!')
-            os.system('say "Slots for delivery opened!"')
-            no_open_slots = False
-            time.sleep(1400)
-      except NoSuchElementException:
-         print('SLOTS OPEN!')
-         os.system('say "Slots for delivery opened!"')
-         no_open_slots = False
-         time.sleep(1400)
-
-
-      try:
-         open_slots = soup.find('div', class_ ='orderSlotExists').text()
-         if open_slots != "false":
-            print('SLOTS OPEN!')
-            os.system('say "Slots for delivery opened!"')
-            no_open_slots = False
-            time.sleep(1400)
-      except AttributeError:
-         pass
-
-      
-
-      
-
+            os.system('say "Slots for delivery opened three!"')
+            #no_open_slots = False
+      time.sleep(30)
 
 getWFSlot('https://www.amazon.com/gp/buy/shipoptionselect/handlers/display.html?hasWorkingJavascript=1')
-
+#getWFSlot('https://www.google.com')
 
